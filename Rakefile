7e93d8353d783a1be598d70336293409e4406990
phaltoe <reverse9@live.com>
2016-07-31 08:52:05 -0300

    created seed file

--- Rakefile
+++ Rakefile
@@ -4,3 +4,17 @@
 require File.expand_path('../config/application', __FILE__)
 
 Rails.application.load_tasks
+
+task :shop_stats => :environment do
+  #what's the total cost of a cart?
+  puts "#{pedros_cart_1.total_cost}"
+  puts "#{avis_cart_1.total_cost}"
+  puts "#{pedros_cart_2.total_cost}"
+
+  #what's the most popular product?
+  puts "Most popular product is: #{Product.most_popular}"
+
+  #who is the most profitable customer?
+  puts "Most profitable customer is: #{Customer.most_profitable}"
+end
+
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+pedro = Customer.create(:name => "Pedro")
+peter = Customer.create(:name => "Peter")
+avi = Customer.create(:name => "Avi")
+
+apple_watch = Product.create(:name => "Apple Watch", :price => "800")
+macbook = Product.create(:name => "Macbook", :price => "1500")
+iphone = Product.create(:name => "Iphone", :price => "1000")
+
+#Let's Pedro buy a macbook
+pedros_cart_1 = pedro.carts.create
+pedros_cart_1.line_items.create(:product => macbook)
+
+#let's Avi buy an iphone and an apple_watch
+avis_cart_1 = avi.carts.create
+avis_cart_1.line_items.create(:product => iphone)
+avis_cart_1.line_items.create(:product => apple_watch)
+
+#let's Pedro buy an iphone in a separate purchase
+pedros_cart_2 = pedro.carts.create
+pedros_cart_2.line_items.create(:product => iphone)
+
+
