43e4e3c376aeb7b25fabd1daff9ac665f8f5ef21
phaltoe <reverse9@live.com>
2016-07-31 15:05:38 -0300

    new methods

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,8 +1,17 @@
 class Customer < ActiveRecord::Base
   has_many :carts
-  # has_many :products, through: :carts
+  has_many :products, through: :carts
 
   def self.who_bought_product_by_name(name)
     joins(:carts => :products).where(:products => {:name => name})
+  end
+
+  def self.summary
+    Customer.includes(:products).all.each do |c|
+      puts "Customer #{c.name} bought:"
+      c.products.each do |p|
+        puts "...#{p.name}"
+      end
+    end
   end
 end
