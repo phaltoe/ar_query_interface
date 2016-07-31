de843b68bd3cad87eebbaa65853a721561801dfb
phaltoe <reverse9@live.com>
2016-07-31 10:05:03 -0300

    created self methods

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -2,4 +2,16 @@ class Cart < ActiveRecord::Base
   belongs_to :customer
   has_many :line_items
   has_many :products, through: :line_items
+
+  def total_cost
+    c.products.sum(:price)
+    # 1 - Find all the line_items in this cart
+    # 2 - Use those to find all the products
+    # 3 - Sum the price of each product
+  end 
+
+  def self.summary
+    #joins(:line_items => :products) == joins(:products)
+    joins(:products).group(:cart_id).sum(:price)
+  end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,13 @@
 class Product < ActiveRecord::Base
   has_many :line_items
   has_many :carts, through: :line_items
+  has_many :customers, through :carts
+
+  def self.most_popular
+    joins(:line_items).group(:product_id).count(:product_id).max
+  end
+
+  def self.bought_this_month
+    joins(:carts).where("carts.created_at" => 1.month.ago..DateTime.now)
+  end
 end
